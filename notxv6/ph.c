04a41e7bec1ef4161634242138f3443293864102
zyksir <jessenykzhu@tencent.com>
2023-05-28 21:03:23 -0700

    finish notxv6 coding

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,7 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t mutex[NBUCKET] = {PTHREAD_MUTEX_INITIALIZER};
 int keys[NKEYS];
 int nthread = 1;
 
@@ -40,13 +41,13 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
   }
+  pthread_mutex_lock(&(mutex[i]));
   if(e){
     // update the existing key.
     e->value = value;
@@ -54,6 +55,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&(mutex[i]));
 
 }
 
@@ -104,6 +106,9 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
+  for(int i = 0; i < NBUCKET; ++i) {
+    pthread_mutex_init(&(mutex[i]), NULL);
+  }
 
 
   if (argc < 2) {
