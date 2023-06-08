2d31141cd2157c69032a3f14719d1aae61277520
zyksir <jessenykzhu@tencent.com>
2023-06-08 00:56:57 -0700

    finish Memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -18,16 +18,22 @@ struct run {
   struct run *next;
 };
 
-struct {
+struct kmem {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+  uint freenum;
+};
+struct kmem kmems[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; ++i) {
+    initlock(&kmems[i].lock, "kmem");
+    kmems[i].freenum = 0;
+  }
   freerange(end, (void*)PHYSTOP);
+  printf("kinit succeed\n");
 }
 
 void
@@ -35,8 +41,9 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -56,10 +63,14 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+  int mycpu_id = cpuid();
+  acquire(&kmems[mycpu_id].lock);
+  r->next = kmems[mycpu_id].freelist;
+  kmems[mycpu_id].freelist = r;
+  kmems[mycpu_id].freenum++;
+  release(&kmems[mycpu_id].lock);
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +81,54 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();
+  int mycpu_id = cpuid();
+  acquire(&kmems[mycpu_id].lock);
+  r = kmems[mycpu_id].freelist;
+  if(r) {
+    kmems[mycpu_id].freelist = r->next;
+    kmems[mycpu_id].freenum--;
+  } else { // steal
+    int max_freenum = 0, max_id = mycpu_id, i = 0;
+    for(i = 0; i < NCPU; ++i) {
+      if (i == mycpu_id) continue;
+      acquire(&kmems[i].lock);
+      if (kmems[i].freenum > max_freenum) {
+        if (max_id != mycpu_id) {
+          release(&kmems[max_id].lock);
+        }
+        max_id = i;
+        max_freenum = kmems[i].freenum;
+      } else {
+        release(&kmems[i].lock);
+      }
+    }
+
+    if (max_freenum == 0) {
+      release(&kmems[mycpu_id].lock);
+      pop_off();
+      return (void*)0;
+    }
+    // printf("cpu %d steal %d from cpu %d, current is %d\n", mycpu_id, max_freenum, max_id, kmems[mycpu_id].freenum);
+
+    struct run* new_tail = kmems[max_id].freelist;
+    kmems[mycpu_id].freelist = new_tail;
+    int new_count = 1;
+    for(; new_count <= max_freenum/2; new_count++) {
+      new_tail = new_tail->next;
+    }
+    kmems[mycpu_id].freenum = new_count;
+    kmems[max_id].freenum -= new_count;
+    kmems[max_id].freelist = new_tail->next;
+    new_tail->next = 0;
+    release(&kmems[max_id].lock);
+    r = kmems[mycpu_id].freelist;
+    kmems[mycpu_id].freelist = r->next;
+    kmems[mycpu_id].freenum--;
+    // printf("after cpu %d freenum %d; cpu %d, freenum %d\n", mycpu_id, kmems[mycpu_id].freenum, max_id, kmems[mycpu_id].freenum);
+  }
+  release(&kmems[mycpu_id].lock);
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
